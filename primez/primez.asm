b574a7c1ab10632313a2d8ddbf6e2971eb45f3ee
Luka Krmpotic <luka.krmpotic@gmail.com>
2023-09-03 10:06:41 +0200

    use macros, add comments, use space not newline

--- primez/primez.asm
+++ primez/primez.asm
@@ -1,64 +1,68 @@
 section .data
+	LIMIT     equ 100
+	STDOUT    equ 1
 	SYS_WRITE equ 1
-	STDOUT equ 1
-	NL db 0xa
+	SYS_EXIT  equ 60
+	ASCII_ZERO  equ 0x30
+	ASCII_SPACE  equ 0x20
 
 section .text
 	global _start
 _start:
-	mov r12, 2
+	mov r12, 2; r12 2..LIMIT
   b:
-	cmp r12, 100
+	cmp r12, LIMIT
 	je exit
 
 	mov rcx, 2
   c:
 	cmp rcx, r12
-	jnl prime
+	jnl prime ; tested all rcx 2..r12, r12 is prime!
+
+	;; is r12 divisible by rcx?
 	mov rax, r12
 	xor rdx, rdx
 	div rcx
 	cmp rdx, 0
-	je back
+	je back ; it is, so r12 not prime!
+
 	inc rcx
 	jmp c
-	
+
   back:
 	inc r12
 	jmp b
 
 prime:
 	mov rax, r12
-	jmp print_rax
+	mov rbx, ASCII_SPACE
+	jmp print_rax_bx
 
-;	jmp print_rax
-;  back:
-
-print_rax:
+print_rax_bx: ; print int in rax, and character in bx
+	push bx
 	mov rbx, 10
 	xor rcx, rcx
-	push bx
 	add rcx, 2
   _pr_0:
 	xor rdx, rdx
 	div rbx
-	add dl, 0x30
+	add dl, ASCII_ZERO
 	push dx
 	add rcx, 2
 	cmp rax, 0
 	jne _pr_0
-	mov rax, 1
-	mov rdi, 1
+	mov rax, SYS_WRITE
+	mov rdi, STDOUT
 	mov rsi, rsp
 	mov rdx, rcx
 	syscall
 	jmp back
 
 exit:
-	mov rax, 60
+	mov rax, SYS_EXIT
 	mov rdi, 0
 	syscall
 error:
-	mov rax, 60
+	mov rax, SYS_EXIT
 	mov rdi, 1
 	syscall
